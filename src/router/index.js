74c582f82bf91aaaf836871d229b8858c4e5372e
atdow <758815713@qq.com>
2022-04-13 00:13:09 +0800

    chore: 修改打包方式

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: atdow
  * @Date: 2021-06-17 10:31:50
  * @LastEditors: null
- * @LastEditTime: 2022-04-04 16:59:32
+ * @LastEditTime: 2022-04-13 00:12:29
  * @Description: file description
  */
 import Vue from "vue";
@@ -12,14 +12,25 @@ Vue.use(VueRouter);
 import navConfig from "@/config/navConfig";
 
 function componentRequire(path, type) {
-  return (r) =>
-    require.ensure(
-      [],
-      () => r(type ? require(`../${path}.${type}`) : require(`../${path}.md`)),
-      "zh-CN"
-    );
+  if (process.env.NODE_ENV === 'development') {
+    // return (resolve) => require([`@/views/${view}`], resolve)
+    return (r) =>
+      require.ensure(
+        [],
+        () => r(type ? require(`@/${path}.${type}`) : require(`../${path}.md`)),
+        () => { },
+        "zh-CN"
+      );
+  } else {
+    // 懒加载方式
+    // let filePath = type ? `@/${path}.${type}` : `@/${path}.md`
+    if (type) {
+      return () => import(`@/${path}.${type}`) // import里面的文件路径必须是字符串（或字符串模板），不能是变量，不然无法分包
+    } else {
+      return () => import(`@/${path}.md`)
+    }
+  }
 }
-
 
 function generateRoute(arr) {
   let route = []
@@ -43,13 +54,11 @@ function generateRoute(arr) {
 }
 let route = generateRoute(navConfig['zh-CN']);
 
-
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/home/index.vue"),
+    component: () => import(/* webpackChunkName: "home" */ "../views/home/index.vue"),
     redirect: { path: route[0].path },
     children: [
       ...route,
@@ -59,6 +68,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
+  // mode: "hash",
   routes,
 });
 
