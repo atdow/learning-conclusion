0a5da20d8511c904dc9a686704748b93bd389176
atdow <758815713@qq.com>
2022-04-14 22:49:33 +0800

    feat: 增加progress组件

---
+++ src/demo/progress.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: atdow
+ * @Date: 2022-04-14 22:14:50
+ * @LastEditors: null
+ * @LastEditTime: 2022-04-14 22:47:47
+ * @Description: file description
+-->
+<template>
+   <div class="progress">
+     <s-progress :percent="10" background="rgb(232, 232, 232)" :height="20" :minWidth="50" direction="left"></s-progress>
+  </div class="progress">
+</template>
+ 
+<script>
+import SProgress from '@/packages/progress'
+export default {
+  name: 'Progress',
+  props: {},
+  data() {
+    return {}
+  },
+  components: {
+    SProgress,
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+}
+</script>
+ 
+<style lang="less" scoped>
+</style>
---
+++ src/packages/progress/src/index.vue
@@ -0,0 +1,99 @@
+<!--
+ * @Author: atdow
+ * @Date: 2022-04-14 22:15:53
+ * @LastEditors: null
+ * @LastEditTime: 2022-04-14 22:47:11
+ * @Description: file description
+-->
+<template>
+  <div
+    v-if="percent"
+    class="s-progress"
+    :style="{
+      width: `calc(100% - ${minWidth}px)`,
+      height: height + 'px',
+      'line-height': height + 'px',
+      background: background,
+      'justify-content': direction === 'left' ? 'flex-start' : 'flex-end',
+    }"
+  >
+    <div
+      class="s-progress-content"
+      :style="{
+        background: strokeColor,
+        width: `calc(${percent}% + ${minWidth}px)`,
+        'text-align': direction === 'right' ? 'left' : 'right',
+        padding: direction === 'right' ? '0 0 0 10px' : '0 10px 0 0 ',
+      }"
+    >
+      <template v-if="$slots.default">
+        <slot></slot>
+      </template>
+      <template v-else> {{ percent }}%</template>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'SProgress',
+  props: {
+    percent: {
+      type: Number,
+      default: 0,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    direction: {
+      type: String,
+      default: 'left',
+      validator: function (value) {
+        return ['left', 'right'].indexOf(value) !== -1
+      },
+    },
+    strokeColor: {
+      type: String,
+      default: 'green',
+    },
+    background: {
+      type: String,
+      default: 'white',
+    },
+    minWidth: {
+      type: Number,
+      default: 0,
+    },
+    height: {
+      type: Number,
+      default: 20,
+    },
+  },
+  data() {
+    return {}
+  },
+  components: {},
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+}
+</script>
+ 
+<style lang="less" scoped>
+.s-progress {
+  color: rgb(232, 232, 232);
+  display: flex;
+  width: 100%;
+  font-size: 12px;
+  color: white;
+  &-content {
+    flex-shrink: 0;
+    text-align: left;
+    box-sizing: border-box;
+    padding-left: 10px;
+  }
+}
+</style>
