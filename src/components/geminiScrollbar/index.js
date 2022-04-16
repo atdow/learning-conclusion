6e17b32afae259e53977ee848b5573090c8f1539
atdow <758815713@qq.com>
2022-04-16 14:01:48 +0800

    ci: test ci

--- src/components/geminiScrollbar/index.js
+++ src/components/geminiScrollbar/index.js
@@ -1,8 +1,15 @@
+/*
+ * @Author: atdow
+ * @Date: 2022-04-01 20:28:26
+ * @LastEditors: null
+ * @LastEditTime: 2022-04-16 14:01:11
+ * @Description: file description
+ */
 import GS from 'gemini-scrollbar'
 import GeminiScrollbar from './geminiScrollbar.vue'
 import CustomScrollbarDerective from './directive'
 
-const install = function(Vue) {
+const install = function (Vue) {
   Vue.component(GeminiScrollbar.name, GeminiScrollbar)
   Vue.directive(CustomScrollbarDerective.name, CustomScrollbarDerective)
   Vue.prototype.$geminiScrollbar = Vue.$geminiScrollbar = GS
@@ -11,6 +18,7 @@ const install = function(Vue) {
 
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
+  // eslint-disable-next-line no-undef
   window[directive.name] = CustomScrollbarDerective
   window.Vue.prototype.$geminiScrollbar = window.Vue.$geminiScrollbar = GS
 }
