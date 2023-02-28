3d2735141944bb10b0a986e9f9269129ef8b44f3
atdow <758815713@qq.com>
2023-02-28 18:50:11 +0800

    feat: 增加scrollUtils工具函数

---
+++ src/utils/scrollUtils.js
@@ -0,0 +1,68 @@
+/*
+ * @Author: atdow
+ * @Date: 2023-02-28 18:41:16
+ * @LastEditors: null
+ * @LastEditTime: 2023-02-28 18:45:08
+ * @Description: file description
+ */
+import raf from 'raf'
+// interface ScrollToOptions {
+//   /** Scroll container, default as window */
+//   getContainer?: () => HTMLElement | Window;
+//   /** Scroll end callback */
+//   callback?: () => any;
+//   /** Animation duration, default as 450 */
+//   duration?: number;
+// }
+
+export const scrollTo = function (y, options = {}) {
+  const { getContainer = () => window, callback, duration = 450 } = options
+
+  const container = getContainer()
+  const scrollTop = getScroll(container, true)
+  const startTime = Date.now()
+
+  const frameFunc = () => {
+    const timestamp = Date.now()
+    const time = timestamp - startTime
+    const nextScrollTop = easeInOutCubic(time > duration ? duration : time, scrollTop, y, duration)
+    if (container === window) {
+      window.scrollTo(window.pageXOffset, nextScrollTop)
+    } else {
+      container.scrollTop = nextScrollTop
+    }
+    if (time < duration) {
+      raf(frameFunc)
+    } else if (typeof callback === 'function') {
+      callback()
+    }
+  }
+  raf(frameFunc)
+}
+
+export const getScroll = function (target, top) {
+  if (typeof window === 'undefined') {
+    return 0
+  }
+
+  const prop = top ? 'pageYOffset' : 'pageXOffset'
+  const method = top ? 'scrollTop' : 'scrollLeft'
+  const isWindow = target === window
+
+  let ret = isWindow ? target[prop] : target[method]
+  // ie6,7,8 standard mode
+  if (isWindow && typeof ret !== 'number') {
+    ret = window.document.documentElement[method]
+  }
+
+  return ret
+}
+
+export const easeInOutCubic = function (t, b, c, d) {
+  const cc = c - b
+  t /= d / 2
+  if (t < 1) {
+    return (cc / 2) * t * t * t + b
+  }
+  return (cc / 2) * ((t -= 2) * t * t + 2) + b
+}
